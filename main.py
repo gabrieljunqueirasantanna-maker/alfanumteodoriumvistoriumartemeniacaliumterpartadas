23a17dabcce817cfb04bd39df575d4fe86467f8c
molotov <molotov@debian.molotov>
2026-01-12 12:18:00 -0300

    feat: suporte a voz, velocidade e pitch

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import FileResponse
 import edge_tts
 import os
 
 app = FastAPI()
+SECRET_KEY = os.getenv("API_KEY")
 
 @app.get("/speak")
-async def speak(text: str, voice: str = "pt-BR-FranciscaNeural"):
+async def speak(
+    text: str, 
+    api_key: str = Query(None), 
+    voice: str = "pt-BR-FranciscaNeural",
+    rate: str = "+0%",
+    pitch: str = "+0Hz"
+):
+    if api_key != SECRET_KEY:
+        raise HTTPException(status_code=403, detail="Chave API invalida")
+        
     output = "audio.mp3"
-    communicate = edge_tts.Communicate(text, voice)
+    # Agora passamos voice, rate e pitch para o motor do Edge
+    communicate = edge_tts.Communicate(text, voice, rate=rate, pitch=pitch)
     await communicate.save(output)
     return FileResponse(output, media_type="audio/mpeg")
